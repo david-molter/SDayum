a4aac6eb281a06c131f7a489ffcc37c7763f75cf
david-molter <david.molter@th-bingen.de>
2024-08-12 17:19:39 +0200

    Erstes Programmieren

---
+++ CORUM.sql
@@ -0,0 +1,10 @@
+Begin;
+    CREATE TABLE complexinfo (
+        complexid PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(250) NOT NULL,
+        synonym VARCHAR (250) NOT Null,
+        organismus VARCHAR(250) NOT Null,
+        cell line VARCHAR (250) NOT NULL);
+
+    CREATE TABLE untereinheiten (
+

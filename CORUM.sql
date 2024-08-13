f5f3ba4080567e6460553fd60bb7e887fc759256
david-molter <david.molter@th-bingen.de>
2024-08-13 10:14:19 +0200

    struggle

--- CORUM.sql
+++ CORUM.sql
@@ -1,61 +1,67 @@
 Begin;
     CREATE TABLE complexinfo (
-        complexid PRIMARY KEY AUTOINCREMENT,
+        complexid INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(250) NOT NULL,
-        synonym VARCHAR (250) NOT Null,
+        synonym VARCHAR(250) NOT Null,
         organismus VARCHAR(250) NOT Null,
-        cell line VARCHAR (250) NOT NULL)
+        cell line VARCHAR(250) NOT NULL,
         sub_id INTEGER NOT NULL,
-            FOREIGN KEY (sub_id) REFERENCES untereinheiten(subid),
+        FOREIGN KEY (sub_id) REFERENCES untereinheiten(subid),
         fun_id INTEGER NOT NULL,
-            FOREIGN KEY (fun_id) REFERENCES funcat(funcatid),
+        FOREIGN KEY (fun_id) REFERENCES funcat(funcatid),
         pub_id INTEGER NOT NULL,
-            FOREIGN KEY (pub_id), REFERENCES publikation(pubid)
-        );
+        FOREIGN KEY (pub_id) REFERENCES publikation(pubid)
+    );
 
     CREATE TABLE untereinheiten (
-        subid PRIMARY KEY AUTOINCREMENT,
+        subid INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(250) NOT NULL,
-        organismus VARCHAR(250( NOT NULL,
+        organismus VARCHAR(250) NOT NULL,
         gen VARCHAR(250) NOT NULL,
         gen_synonym VARCHAR(250) NOT NULL,
         uniprot_id VARCHAR(250) NOT NULL,
-        entrez_id VARCHAR (250) NOT NULL,
+        entrez_id VARCHAR(250) NOT NULL,
         complex_id INTEGER NOT NULL,
-            FOREIGN KEY (complex_id) REFERENCES complexinfo(complexid)
+        FOREIGN KEY (complex_id) REFERENCES complexinfo(complexid),
         pub_id INTEGER NOT NULL,
-            FOREIGN KEY (pub_id) REFERENCES publikation(pubid)
+        FOREIGN KEY (pub_id) REFERENCES publikation(pubid)
         );
 
     CREATE TABLE funktion (
-        funid PRIMARY KEY AUTOINCREMENT,
+        funid INTEGER PRIMARY KEY AUTOINCREMENT,
         complex_id INTEGER NOT NULL,
-            FOREIGN KEY (complex_id) REFERENCES complexinfo(complexid),
+        FOREIGN KEY (complex_id) REFERENCES complexinfo(complexid),
         funcat_id INTEGER NOT NULL,
-            FOREIGN KEY (funcat_id) REFERENCES funcat(funcatid),
+        FOREIGN KEY (funcat_id) REFERENCES funcat(funcatid),
         go_id INTEGER NOT NULL,
-            FOREIGN KEY (go_id) REFERENCES go(goid)
+        FOREIGN KEY (go_id) REFERENCES go(goid)
         );
 
     CREATE TABLE funcat (
-        funcatid PRIMARY KEY AUTOINCREMENT,
+        funcatid INTEGER PRIMARY KEY AUTOINCREMENT,
         fundescription VARCHAR(250) NOT NULL
         );
 
     CREATE TABLE go (
-        goid PRIMARY KEY AUTOINCREMENT,
+        goid INTEGER PRIMARY KEY AUTOINCREMENT,
         godescription VARCHAR(250) NOT NULL
         );
 
     CREATE TABLE publikation (
-        pubid PRIMARY KEY AUTOINCREMENT,
+        pubid INTEGER PRIMARY KEY AUTOINCREMENT,
         purification VARCHAR(250) NOT NULL,
         complex_comment VARCHAR(250) NOT NULL,
         disease_comment VARCHAR(250) NOT NULL,
         sub_comment VARCHAR(250) NOT NULL,
         pubmed_id VARCHAR(250) NOT NULL,
         complex_id INTEGER NOT NULL,
-            FOREIGN KEY (complex_id) REFERENCES complexinfo(complexid),
+        FOREIGN KEY (complex_id) REFERENCES complexinfo(complexid),
         sub_id INTEGER NOT NULL,
-            FOREIGN KEY (sub_id) REFERENCES untereinheiten(subid)
+        FOREIGN KEY (sub_id) REFERENCES untereinheiten(subid)
         );
+
+-- Definition der Relationen zwischen den oben eingefügten Datenmodellen
+
+
+
+COMMIT;

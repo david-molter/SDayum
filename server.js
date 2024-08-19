3cffd90787d764465984b4173d269efdfad38936
david-molter <david.molter@th-bingen.de>
2024-08-19 13:42:49 +0200

    server änderung

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//Implementieren des Express-Frameworks für den Server
 const express = require('express')
 
+//Konfiguration eines Express-Servers (app) um JSON- und URL-codierte Daten zu parsen
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
 
+//Verbindung zu SQLite-Datenbanken
+const sqite = require('sqlite')
+const sqlite3 = require('sqlite3')
+
+//Rendern von HTML-Vorlagen
+const ejs = require('eja')

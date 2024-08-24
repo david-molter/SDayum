d609e9d8418d4e26980654648552c3aca027a083
david-molter <david.molter@th-bingen.de>
2024-08-24 09:58:51 +0200

    searchArg

--- searchArg.js
+++ searchArg.js
@@ -1,3 +1,54 @@
+function testTranslateToSQL() {
+    var example_search_arg_1 = {
+        descendants: [{
+            field: "name",
+            op: "like",
+            val: "B%"
+        },
+        {
+            field: "organismus",
+            op: "==",
+            val: "Human"
+        }
+    ],
+    op: "AND"
+    };
+
+    var example_search_arg_2 = {
+        op: "AND",
+        descendants: [{
+            op: "OR",
+            descendants: [{
+                field: "name",
+                op: "like",
+                val: "B%"
+            },
+            {
+                field: "organismus",
+                op: "=",
+                val: "Human"
+            }
+        ]
+    },
+    {
+        field: "is_Human",
+        op: "==",
+        val: true
+    }
+    ]
+    };
+    const expected_sql_example_1 =
+    'SELECT * FROM Proteinkomplexe WHERE ( (name like B%) AND (organismus == Human) )';
+    const test_1 = translateToSQL(example_search_arg_1) === expected_sql_example_1;
+
+    return test_1;
+}
+
+const fieldTypes = {
+    compexid: "int",
+    
+}
+
 function translateSearchTripletToSQL(triplet) {
     const { field, operator, value } = searchArg;
     return `${field} ${operator} '${value}'`;
@@ -20,4 +71,4 @@ function translateToSQLRecursive(searchArg) {
     }
 }
  
-module.exports = {translateToSQL};
+module.exports = {translateToSQL, testTranslateToSQL};
